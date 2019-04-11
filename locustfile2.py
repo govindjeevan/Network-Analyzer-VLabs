3d6c1f01e1caee2594ac1c2fdf6fb15666d522db
smarty1palak <smarty1palak@gmail.com>
2019-04-11 19:31:05 +0530

    some mistakes fixed

---
+++ locustfile2.py
@@ -0,0 +1,23 @@
+from locust import HttpLocust, TaskSet, task
+from locust import events
+
+class UserBehavior(TaskSet):
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+
+    def on_stop(self):
+        """ on_stop is called when the TaskSet is stopping """
+
+    @task(1)
+    def index(self):
+        with open('codepart.txt') as f:
+            content = f.read().splitlines()
+        f = open("global.txt", "r")
+        self.client.get(content[0])
+    
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
--- vlab1.sh
+++ vlab1.sh
@@ -1,4 +1,4 @@
 python test.py
 read -r line < segmented1.txt
 echo "${line}"
-locust -f locustfile1 --host=$line
+locust -f locustfile2 --host=$line
